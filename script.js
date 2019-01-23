c8ce30d277250ed8a788b272efa87c739326bcaa
Martin Janka <ift@m-janka.de>
2019-01-23 10:26:07 +0100

    JSON Datei erstellt, Datenzugriff auf JSON von JavaScript klappt noch nicht

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 'use strict';
 console.log("Script läuft");
 
-
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+  }
+};
+xmlhttp.open("GET", "data.json", true);
+xmlhttp.send();
