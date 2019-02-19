ab0fd1a667863333f9d645774c0c4c0a3fc03815
Martin Janka <ift@m-janka.de>
2019-02-19 13:20:45 +0100

    V1

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,41 @@
-'use strict';
+(function () {
+    'use strict';
+    //========================== Service Worker ==================
+    const regServiceWorker = function () {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('./service-worker.js')
+                .then(function () {
+                    console.log('Service Worker wurde registriert');
+                });
+        } else {
+            console.log('Browser bietet keine Unterstützung für Service Worker');
+        }
+    };
+    //========================== Model ==================
 
-if('serviceWorker' in navigator){
-    navigator.serviceWorker
-    .register('./service-worker.js')
-    .then(function(){
-        console.log('Service Worker wurde registriert');
-    });
-} else {
-    console.log('Browser bietet keine Unterstützung für Service Worker');
-}
+
+    //========================== Presenter ==================
+    const presenter = {
+        init: function () {
+            //Eventhandler registieren
+            const btnAdd = document.getElementById('add');
+            const btnEdit = document.getElementById('edit');
+            const btnDel = document.getElementById('del');
+        }
+    };
+
+    //========================== View ==================
+
+    //========================== App ==================
+    presenter.init();
+})();
+
+
+
+
+
+
+
+
+
