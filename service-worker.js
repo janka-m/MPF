ec643edb6629df49a78512242160ac1a47e42852
Martin Janka <ift@m-janka.de>
2019-02-19 11:48:17 +0100

    Service Worker hinzugefügt

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+
+// Daten
+let cacheName = 'MPF';
+let filesToCache = [
+    './',
+    './index.html',
+    './JS/script.js',
+    './service-worker.js'
+];
+// Dateien in Cache speichern
+self.addEventListener('install', function (event) {
+    console.log('[Service Worker] Installation wird gestartet');
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Dateien werden gecachet');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+// Dateien vom Cache laden
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Angeforderte URL: ', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('[Service Worker] Ressource aus dem Cache geladen: ', event.request.url);
+                return response;
+            } else {
+                console.log('[Service Worker] Ressource aus dem Netzwerk geladen: ', event.request.url);
+                return fetch(event.request);
+            }
+        })
+    );
+});
