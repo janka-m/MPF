9f6901c19292afdaece51a99075593cb8373d96d
Martin Janka <ift@m-janka.de>
2019-02-26 12:46:30 +0100

    Klickbare Liste hinzugefügt

--- JS/script.js
+++ JS/script.js
@@ -15,26 +15,24 @@
     //========================== Model ==================================
     const model = {
         datenArray: [{
-            Restaurants: [{
-                    name: "Metzgerwirt",
-                    adresse: "Poststraße 10, 86857 Hurlach",
-                    telefon: "08248 7676",
-                    internet: "www.beim-metzgerwirt.de"
-                },
-                {
-                    name: "Martha Pizzarei",
-                    adresse: "Hauptstraße 11, 82256 Fürstenfeldbruck",
-                    telefon: "08141 3566747",
-                    internet: "martha-pizzarei.de"
-                },
-                {
-                    name: "Gasthof Alter Wirt",
-                    adresse: "Moorenweiser Straße 5, 82269 Geltendorf",
-                    telefon: "08193 7454",
-                    internet: null
-                }
-            ]
-        }],
+                name: "Metzgerwirt",
+                adresse: "Poststraße 10, 86857 Hurlach",
+                telefon: "08248 7676",
+                internet: "www.beim-metzgerwirt.de"
+            },
+            {
+                name: "Martha Pizzarei",
+                adresse: "Hauptstraße 11, 82256 Fürstenfeldbruck",
+                telefon: "08141 3566747",
+                internet: "martha-pizzarei.de"
+            },
+            {
+                name: "Gasthof Alter Wirt",
+                adresse: "Moorenweiser Straße 5, 82269 Geltendorf",
+                telefon: "08193 7454",
+                internet: ""
+            }
+        ],
 
         // READ
         readAll: function () {
@@ -68,7 +66,39 @@
                 this.anzeigeNode.removeChild(this.anzeigeNode.firstChild);
             }
             //Neue Liste hinzufügen
+            const divNode = document.createElement('div');
+            divNode.classList.add('collection');
+
+            for (let index = 0; index < daten.length; index++) {
+                const name = daten[index].name;
+                // const adresse = daten[index].adresse;
+                // const telefon = daten[index].telefon;
+                // const internet = daten[index].internet;
+
+                const aNode = document.createElement('a');
+                aNode.setAttribute('href', '#!');
+                aNode.setAttribute('class', 'collection-item');
+
+                const aTextNode = document.createTextNode(name);
+
+                aNode.appendChild(aTextNode);
+                divNode.appendChild(aNode);
+                this.anzeigeNode.appendChild(divNode);
+
+
+
+
+
+
+
+
+
+            }
+
+
         }
+
+
     }
     //========================== App ====================================
     presenter.init();
