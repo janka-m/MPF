27dd8fd8a49fecff978de00d51c3cc48a76e02a6
Martin Janka <ift@m-janka.de>
2019-04-09 11:42:57 +0200

    Eingabemaske für ADD-Button hinterlegt

--- JS/script.js
+++ JS/script.js
@@ -62,6 +62,10 @@
         backButtonClick: function () {
             let daten = model.readAll();
             view.renderList(daten);
+        },
+        //------------------------------ presenter.neuButtonClick ------------------------------
+        neuButtonClick: function () {
+            view.renderNew();
         }
     };
 
@@ -79,12 +83,17 @@
             while (node.firstChild) {
                 node.removeChild(node.firstChild);
             }
+            node.removeAttribute('class');
         },
         //------------------------------ view.renderList ------------------------------
         renderList: function (daten) {
             // Gesamte View löschen 
             view.loeschen(this.anzeigeNode);
             view.loeschen(this.buttonNode);
+
+            // Attribut für Anzeige  DIV setzten
+            this.anzeigeNode.setAttribute('class', 'collection');
+
             // Neue Liste erzeugen 
             for (let index = 0; index < daten.length; index++) {
                 const name = daten[index].name;
@@ -110,7 +119,7 @@
             const iNodeButtonAddText = document.createTextNode('+');
             // Eventlistener Add Button
             iNodeButtonAdd.addEventListener('click', function () {
-                console.log('Button Add');
+                presenter.neuButtonClick();
             });
             // Add Button zusammensetzen
             iNodeButtonAdd.appendChild(iNodeButtonAddText);
@@ -122,6 +131,7 @@
             // gesamte View löschen
             view.loeschen(this.anzeigeNode);
             view.loeschen(this.buttonNode);
+
             // Personendetails ausgeben
             const ulNode = document.createElement('ul');
             ulNode.setAttribute('class', 'collection with-header');
@@ -148,17 +158,17 @@
             const telefonTextNode = document.createTextNode(daten.telefon);
             const internetTextNode = document.createTextNode(daten.internet);
 
-            aNodeInternet.appendChild(internetTextNode);
-
             liNodeName.appendChild(nameTextNode);
             liNodeAdresse.appendChild(adresseTextNode);
             liNodeTelefon.appendChild(telefonTextNode);
-            liNodeInternet.appendChild(aNodeInternet);
+            liNodeInternet.appendChild(internetTextNode);
+
+            aNodeInternet.appendChild(liNodeInternet);
 
             ulNode.appendChild(liNodeName);
             ulNode.appendChild(liNodeAdresse);
             ulNode.appendChild(liNodeTelefon);
-            ulNode.appendChild(liNodeInternet);
+            ulNode.appendChild(aNodeInternet);
 
             this.anzeigeNode.appendChild(ulNode);
 
@@ -193,6 +203,137 @@
             iNodeButtonDel.appendChild(iNodeButtonDelText);
             aNodeButtonDel.appendChild(iNodeButtonDel);
             this.buttonNode.appendChild(aNodeButtonDel);
+
+            // Back Button erzeugen
+            const aNodeButtonBack = document.createElement('a');
+            aNodeButtonBack.setAttribute('class', 'btn-floating btn-large waves-effect waves-light blue-grey lighten-4')
+            aNodeButtonBack.setAttribute('id', 'back');
+            aNodeButtonBack.setAttribute('style', 'float:left');
+            const iNodeButtonBack = document.createElement('i');
+            iNodeButtonBack.setAttribute('class', 'material-icons');
+            const iNodeButtonBackText = document.createTextNode('back');
+            // Eventlistener Back Button
+            iNodeButtonBack.addEventListener('click', function () {
+                presenter.backButtonClick();
+            });
+            // Back Button Del zusammensetzen
+            iNodeButtonBack.appendChild(iNodeButtonBackText);
+            aNodeButtonBack.appendChild(iNodeButtonBack);
+            this.buttonNode.appendChild(aNodeButtonBack);
+        },
+        renderNew: function () {
+            // gesamte View löschen
+            view.loeschen(this.anzeigeNode);
+            view.loeschen(this.buttonNode);
+
+            // Attribut für Anzeige  DIV setzten
+            this.anzeigeNode.setAttribute('class', 'row');
+
+            // FormNode erzeugen:            
+            const formNode = document.createElement('form');
+            formNode.setAttribute('class', 'col s12 offset-m3');
+            
+
+            // Name: Elemente erzeugen
+            const divNodeRowName = document.createElement('div');
+            divNodeRowName.setAttribute('class', 'row');
+            const divNodeInputName = document.createElement('div');
+            divNodeInputName.setAttribute('class', 'input-field col s6');
+            const inputNodeName = document.createElement('input');
+            inputNodeName.setAttribute('id', 'inputName');
+            inputNodeName.setAttribute('type', 'text');
+            inputNodeName.setAttribute('class', 'validate');
+            const inputLabelName = document.createElement('label');
+            inputLabelName.setAttribute('for', 'inputName');
+            inputLabelName.setAttribute('class', 'active');
+            const textNodeName = document.createTextNode('Name');
+            //Name: Elemente zusammenzetzen
+            inputLabelName.appendChild(textNodeName);
+            divNodeInputName.appendChild(inputNodeName);
+            divNodeInputName.appendChild(inputLabelName);
+            divNodeRowName.appendChild(divNodeInputName);
+
+            // Adresse: Elemente erzeugen
+            const divNodeRowAdresse = document.createElement('div');
+            divNodeRowAdresse.setAttribute('class', 'row');
+            const divNodeInputAdresse = document.createElement('div');
+            divNodeInputAdresse.setAttribute('class', 'input-field col s6');
+            const inputNodeAdresse = document.createElement('input');
+            inputNodeAdresse.setAttribute('id', 'inputAdresse');
+            inputNodeAdresse.setAttribute('type', 'text');
+            inputNodeAdresse.setAttribute('class', 'validate');
+            const inputLabelAdresse = document.createElement('label');
+            inputLabelAdresse.setAttribute('for', 'inputAdresse');
+            inputLabelAdresse.setAttribute('class', 'active');
+            const textNodeAdresse = document.createTextNode('Adresse');
+            //Adresse: Elemente zusammenzetzen
+            inputLabelAdresse.appendChild(textNodeAdresse);
+            divNodeInputAdresse.appendChild(inputNodeAdresse);
+            divNodeInputAdresse.appendChild(inputLabelAdresse);
+            divNodeRowAdresse.appendChild(divNodeInputAdresse);
+
+            // Telefon: Elemente erzeugen
+            const divNodeRowTelefon = document.createElement('div');
+            divNodeRowTelefon.setAttribute('class', 'row');
+            const divNodeInputTelefon = document.createElement('div');
+            divNodeInputTelefon.setAttribute('class', 'input-field col s6');
+            const inputNodeTelefon = document.createElement('input');
+            inputNodeTelefon.setAttribute('id', 'inputTelefon');
+            inputNodeTelefon.setAttribute('type', 'text');
+            inputNodeTelefon.setAttribute('class', 'validate');
+            const inputLabelTelefon = document.createElement('label');
+            inputLabelTelefon.setAttribute('for', 'inputTelefon');
+            inputLabelTelefon.setAttribute('class', 'active');
+            const textNodeTelefon = document.createTextNode('Telefon');
+            //Telefon: Elemente zusammenzetzen
+            inputLabelTelefon.appendChild(textNodeTelefon);
+            divNodeInputTelefon.appendChild(inputNodeTelefon);
+            divNodeInputTelefon.appendChild(inputLabelTelefon);
+            divNodeRowTelefon.appendChild(divNodeInputTelefon);
+
+            // Internet: Elemente erzeugen
+            const divNodeRowInternet = document.createElement('div');
+            divNodeRowInternet.setAttribute('class', 'row');
+            const divNodeInputInternet = document.createElement('div');
+            divNodeInputInternet.setAttribute('class', 'input-field col s6');
+            const inputNodeInternet = document.createElement('input');
+            inputNodeInternet.setAttribute('id', 'inputInternet');
+            inputNodeInternet.setAttribute('type', 'text');
+            inputNodeInternet.setAttribute('class', 'validate');
+            const inputLabelInternet = document.createElement('label');
+            inputLabelInternet.setAttribute('for', 'inputInternet');
+            inputLabelInternet.setAttribute('class', 'active');
+            const textNodeInternet = document.createTextNode('Internet');
+            //Internet: Elemente zusammenzetzen
+            inputLabelInternet.appendChild(textNodeInternet);
+            divNodeInputInternet.appendChild(inputNodeInternet);
+            divNodeInputInternet.appendChild(inputLabelInternet);
+            divNodeRowInternet.appendChild(divNodeInputInternet);
+
+            // Einzelne Elemente zur FormNode hinzufügen
+            formNode.appendChild(divNodeRowName);
+            formNode.appendChild(divNodeRowAdresse);
+            formNode.appendChild(divNodeRowTelefon);
+            formNode.appendChild(divNodeRowInternet);
+
+            // FormNode der Anzeige hinzufügen
+            this.anzeigeNode.appendChild(formNode);
+
+            // SpeichernNeu Button erzeugen
+            const aNodeButtonSpeichernNeu = document.createElement('a');
+            aNodeButtonSpeichernNeu.setAttribute('class', 'btn-floating btn-large waves-effect waves-light green lighten-2')
+            aNodeButtonSpeichernNeu.setAttribute('id', 'speichernNew');
+            const iNodeButtonSpeichernNeu = document.createElement('i');
+            iNodeButtonSpeichernNeu.setAttribute('class', 'material-icons');
+            const iNodeButtonSpeichernNeuText = document.createTextNode('rec');
+            // Eventlistener Edit Button
+            iNodeButtonSpeichernNeu.addEventListener('click', function () {
+                console.log('Button SpeichernNeu');
+            });
+            // Edit Button zusammensetzen
+            iNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeuText);
+            aNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeu);
+            this.buttonNode.appendChild(aNodeButtonSpeichernNeu);
 
             // Back Button erzeugen
             const aNodeButtonBack = document.createElement('a');
