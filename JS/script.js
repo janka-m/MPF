03d57be323dd96583b8000adaac3158d9aa83258
Martin Janka <ift@m-janka.de>
2019-04-17 14:52:37 +0200

    Funktionalität OHNE Überprüfungen fertig. Offline Synchronisation und Installation aufs Smartphone fehlen auch noch

--- JS/script.js
+++ JS/script.js
@@ -15,28 +15,15 @@
     };
     //======================================== Model ========================================
     const model = {
-        datenArray: [{
-                name: "Metzgerwirt",
-                adresse: "Poststraße 10, 86857 Hurlach",
-                telefon: "08248 7676",
-                url: "https://www.beim-metzgerwirt.de"
-            },
-            {
-                name: "Martha Pizzarei",
-                adresse: "Hauptstraße 11, 82256 Fürstenfeldbruck",
-                telefon: "08141 3566747",
-                url: "https://www.martha-pizzarei.de"
-            },
-            {
-                name: "Gasthof Alter Wirt",
-                adresse: "Moorenweiser Straße 5, 82269 Geltendorf",
-                telefon: "08193 7454",
-                url: ""
-            }
-        ],
+        datenArray: [],
+        //------------------------------ model.init ------------------------------
+        init: function(){
+            this.datenArray = dao.lesen();
+        },
         //------------------------------ model.Create ------------------------------
         create: function (eintrag) {
             this.datenArray.push(eintrag);
+            dao.schreiben(this.datenArray);
         },
         //------------------------------ model.readAll ------------------------------
         readAll: function () {
@@ -45,6 +32,16 @@
         //------------------------------ model.readOne ------------------------------
         readOne: function (index) {
             return this.datenArray[index];
+        },
+        //------------------------------ model.update ------------------------------
+        update: function (eintrag, index) {
+            this.datenArray[index] = eintrag;
+            dao.schreiben(this.datenArray);
+        },
+        //------------------------------ model.delete ------------------------------
+        delete: function (index) {
+            this.datenArray.splice(index, 1);
+            dao.schreiben(this.datenArray);
         }
     };
 
@@ -52,15 +49,17 @@
     const presenter = {
         //------------------------------ presenter.init ------------------------------
         init: function () {
-            // Daten (Restaurants) ausgeben
+            // Daten vom LocalStorage aktuallisieren
+            model.init();
             let daten = model.readAll();
+            // Daten (Restaurants) ausgeben
             view.init();
             view.renderList(daten);
         },
         //------------------------------ presenter.listElementClick ------------------------------
         listElementClick: function (index) {
             let daten = model.readOne(index);
-            view.renderOne(daten);
+            view.renderOne(daten, index);
         },
         //------------------------------ presenter.btnBackClick ------------------------------
         btnBackClick: function () {
@@ -71,8 +70,8 @@
         btnAddClick: function () {
             view.renderNew();
         },
-        //------------------------------ presenter.btnAddNeuClick ------------------------------
-        btnAddNeuClick: function () {
+        //------------------------------ presenter.btnAddSpeichernClick ------------------------------
+        btnAddSpeichernClick: function () {
             // Daten aus dem Formular holen
             let name = view.getName();
             let adresse = view.getAdresse();
@@ -91,6 +90,35 @@
             let daten = model.readAll();
 
             // Liste erneut anzeigen mit neuen Daten;
+            view.renderList(daten);
+        },
+        //------------------------------ presenter.btnEditClick ------------------------------
+        btnEditClick: function (daten, index) {
+            view.renderEdit(daten, index);
+        },
+        //------------------------------ presenter.btnEditSpeichernClick ------------------------------
+        btnEditSpeichernClick: function (index) {
+            // Daten aus dem Formular holen
+            let eintrag = {};
+            eintrag.name = view.getName();
+            eintrag.adresse = view.getAdresse();
+            eintrag.telefon = view.getTelefon();
+            eintrag.url = view.getURL();
+
+            // alten Datensatz überschreiben
+            model.update(eintrag, index);
+
+            // Daten erneut holen
+            let daten = model.readAll();
+
+            // Liste erneut anzeigen mit neuen Daten;
+            view.renderList(daten);
+        },
+        //------------------------------ presenter.btnDelClick ------------------------------
+        btnDelClick: function (index) {
+            // Datensatz löschen
+            model.delete(index);
+            let daten = model.readAll();
             view.renderList(daten);
         }
 
@@ -155,7 +183,7 @@
             this.buttonNode.appendChild(aNodeButtonAdd);
         },
         //------------------------------ view.renderOne ------------------------------
-        renderOne: function (daten) {
+        renderOne: function (daten, index) {
             // gesamte View löschen
             view.loeschen(this.anzeigeNode);
             view.loeschen(this.buttonNode);
@@ -209,7 +237,7 @@
             const iNodeButtonEditText = document.createTextNode('edit');
             // Eventlistener Edit Button
             iNodeButtonEdit.addEventListener('click', function () {
-                console.log('Button Edit');
+                presenter.btnEditClick(daten, index);
             });
             // Edit Button zusammensetzen
             iNodeButtonEdit.appendChild(iNodeButtonEditText);
@@ -225,7 +253,7 @@
             const iNodeButtonDelText = document.createTextNode('del');
             // Eventlistener Del Button
             iNodeButtonDel.addEventListener('click', function () {
-                console.log('Button Del');
+                presenter.btnDelClick(index);
             });
             // Del Button Del zusammensetzen
             iNodeButtonDel.appendChild(iNodeButtonDelText);
@@ -354,23 +382,23 @@
             // Focus auf InputName
             inputName.focus();
 
-            // SpeichernNeu Button erzeugen
+            // NeuSpeichern Button erzeugen
             const aNodeButtonSpeichernNeu = document.createElement('a');
             aNodeButtonSpeichernNeu.setAttribute('class', 'btn-floating btn-large waves-effect waves-light green lighten-2')
             aNodeButtonSpeichernNeu.setAttribute('id', 'speichernNew');
             const iNodeButtonSpeichernNeu = document.createElement('i');
             iNodeButtonSpeichernNeu.setAttribute('class', 'material-icons');
             const iNodeButtonSpeichernNeuText = document.createTextNode('+');
-            // Eventlistener Edit Button
+            // Eventlistener NeuSpeichernButton
             iNodeButtonSpeichernNeu.addEventListener('click', function () {
-                presenter.btnAddNeuClick();
+                presenter.btnAddSpeichernClick();
             });
-            // Edit Button zusammensetzen
+            // NeuSpeichernButton zusammensetzen
             iNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeuText);
             aNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeu);
             this.buttonNode.appendChild(aNodeButtonSpeichernNeu);
 
-            // Back Button erzeugen
+            // BackButton erzeugen
             const aNodeButtonBack = document.createElement('a');
             aNodeButtonBack.setAttribute('class', 'btn-floating btn-large waves-effect waves-light blue-grey lighten-4')
             aNodeButtonBack.setAttribute('id', 'back');
@@ -378,11 +406,153 @@
             const iNodeButtonBack = document.createElement('i');
             iNodeButtonBack.setAttribute('class', 'material-icons');
             const iNodeButtonBackText = document.createTextNode('back');
-            // Eventlistener Back Button
+            // EventlistenerBack Button
             iNodeButtonBack.addEventListener('click', function () {
                 presenter.btnBackClick();
             });
-            // Back Button Del zusammensetzen
+            // BackButton Del zusammensetzen
+            iNodeButtonBack.appendChild(iNodeButtonBackText);
+            aNodeButtonBack.appendChild(iNodeButtonBack);
+            this.buttonNode.appendChild(aNodeButtonBack);
+        },
+        //------------------------------ view.renderEdit ------------------------------
+        renderEdit: function (daten, index) {
+            // gesamte View löschen
+            view.loeschen(this.anzeigeNode);
+            view.loeschen(this.buttonNode);
+
+            // Attribut für Anzeige  DIV setzten
+            this.anzeigeNode.setAttribute('class', 'row');
+            this.anzeigeNode.setAttribute('style', 'margin:0 10%');
+
+            // FormNode erzeugen:            
+            const formNode = document.createElement('form');
+            formNode.setAttribute('class', 'col s12');
+
+            // Name: Elemente erzeugen
+            const divNodeRowName = document.createElement('div');
+            divNodeRowName.setAttribute('class', 'row');
+            const divNodeInputName = document.createElement('div');
+            divNodeInputName.setAttribute('class', 'input-field col s12');
+            this.inputName = document.createElement('input');
+            this.inputName.value = daten.name;
+            this.inputName.setAttribute('id', 'inputName');
+            this.inputName.setAttribute('type', 'text');
+            this.inputName.setAttribute('class', 'validate');
+            const inputLabelName = document.createElement('label');
+            inputLabelName.setAttribute('for', 'inputName');
+            inputLabelName.setAttribute('class', 'active');
+            const textNodeName = document.createTextNode('Name');
+            //Name: Elemente zusammensetzen
+            inputLabelName.appendChild(textNodeName);
+            divNodeInputName.appendChild(this.inputName);
+            divNodeInputName.appendChild(inputLabelName);
+            divNodeRowName.appendChild(divNodeInputName);
+
+            // Adresse: Elemente erzeugen
+            const divNodeRowAdresse = document.createElement('div');
+            divNodeRowAdresse.setAttribute('class', 'row');
+            const divNodeInputAdresse = document.createElement('div');
+            divNodeInputAdresse.setAttribute('class', 'input-field col s12');
+            this.inputAdresse = document.createElement('input');
+            this.inputAdresse.value = daten.adresse;
+            this.inputAdresse.setAttribute('placeholder', 'Musterstraße 1, 81818 Musterstadt')
+            this.inputAdresse.setAttribute('id', 'inputAdresse');
+            this.inputAdresse.setAttribute('type', 'text');
+            this.inputAdresse.setAttribute('class', 'validate');
+            const inputLabelAdresse = document.createElement('label');
+            inputLabelAdresse.setAttribute('for', 'inputAdresse');
+            inputLabelAdresse.setAttribute('class', 'active');
+            const textNodeAdresse = document.createTextNode('Adresse');
+            //Adresse: Elemente zusammensetzen
+            inputLabelAdresse.appendChild(textNodeAdresse);
+            divNodeInputAdresse.appendChild(this.inputAdresse);
+            divNodeInputAdresse.appendChild(inputLabelAdresse);
+            divNodeRowAdresse.appendChild(divNodeInputAdresse);
+
+            // Telefon: Elemente erzeugen
+            const divNodeRowTelefon = document.createElement('div');
+            divNodeRowTelefon.setAttribute('class', 'row');
+            const divNodeInputTelefon = document.createElement('div');
+            divNodeInputTelefon.setAttribute('class', 'input-field col s12');
+            this.inputTelefon = document.createElement('input');
+            this.inputTelefon.value = daten.telefon;
+            this.inputTelefon.setAttribute('placeholder', '+49 1234 56789')
+            this.inputTelefon.setAttribute('id', 'inputTelefon');
+            this.inputTelefon.setAttribute('type', 'tel');
+            this.inputTelefon.setAttribute('class', 'validate');
+            const inputLabelTelefon = document.createElement('label');
+            inputLabelTelefon.setAttribute('for', 'inputTelefon');
+            inputLabelTelefon.setAttribute('class', 'active');
+            const textNodeTelefon = document.createTextNode('Telefon');
+            //Telefon: Elemente zusammensetzen
+            inputLabelTelefon.appendChild(textNodeTelefon);
+            divNodeInputTelefon.appendChild(this.inputTelefon);
+            divNodeInputTelefon.appendChild(inputLabelTelefon);
+            divNodeRowTelefon.appendChild(divNodeInputTelefon);
+
+            // URL: Elemente erzeugen
+            const divNodeRowURL = document.createElement('div');
+            divNodeRowURL.setAttribute('class', 'row');
+            const divNodeInputURL = document.createElement('div');
+            divNodeInputURL.setAttribute('class', 'input-field col s12');
+            this.inputURL = document.createElement('input');
+            this.inputURL.value = daten.url;
+            this.inputURL.setAttribute('placeholder', 'https://www.google.de')
+            this.inputURL.setAttribute('id', 'inputURL');
+            this.inputURL.setAttribute('type', 'url');
+            this.inputURL.setAttribute('class', 'validate');
+            const inputLabelURL = document.createElement('label');
+            inputLabelURL.setAttribute('for', 'inputURL');
+            inputLabelURL.setAttribute('class', 'active');
+            const textNodeURL = document.createTextNode('Webadresse');
+            //URL: Elemente zusammensetzen
+            inputLabelURL.appendChild(textNodeURL);
+            divNodeInputURL.appendChild(this.inputURL);
+            divNodeInputURL.appendChild(inputLabelURL);
+            divNodeRowURL.appendChild(divNodeInputURL);
+
+            // Einzelne Elemente zur FormNode hinzufügen
+            formNode.appendChild(divNodeRowName);
+            formNode.appendChild(divNodeRowAdresse);
+            formNode.appendChild(divNodeRowTelefon);
+            formNode.appendChild(divNodeRowURL);
+
+            // FormNode der Anzeige hinzufügen
+            this.anzeigeNode.appendChild(formNode);
+
+            // Focus auf InputName
+            inputName.focus();
+
+            // EditSpeichern Button erzeugen
+            const aNodeButtonSpeichernNeu = document.createElement('a');
+            aNodeButtonSpeichernNeu.setAttribute('class', 'btn-floating btn-large waves-effect waves-light blue lighten-2')
+            aNodeButtonSpeichernNeu.setAttribute('id', 'speichernNew');
+            const iNodeButtonSpeichernNeu = document.createElement('i');
+            iNodeButtonSpeichernNeu.setAttribute('class', 'material-icons');
+            const iNodeButtonSpeichernNeuText = document.createTextNode('+');
+            // Eventlistener EditSpeichernButton
+            iNodeButtonSpeichernNeu.addEventListener('click', function () {
+                presenter.btnEditSpeichernClick(index);
+            });
+            // EditSpeichernButton zusammensetzen
+            iNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeuText);
+            aNodeButtonSpeichernNeu.appendChild(iNodeButtonSpeichernNeu);
+            this.buttonNode.appendChild(aNodeButtonSpeichernNeu);
+
+            // BackButton erzeugen
+            const aNodeButtonBack = document.createElement('a');
+            aNodeButtonBack.setAttribute('class', 'btn-floating btn-large waves-effect waves-light blue-grey lighten-4')
+            aNodeButtonBack.setAttribute('id', 'back');
+            aNodeButtonBack.setAttribute('style', 'float:left');
+            const iNodeButtonBack = document.createElement('i');
+            iNodeButtonBack.setAttribute('class', 'material-icons');
+            const iNodeButtonBackText = document.createTextNode('back');
+            // Eventlistener BackButton
+            iNodeButtonBack.addEventListener('click', function () {
+                presenter.btnBackClick();
+            });
+            // BackButton zusammensetzen
             iNodeButtonBack.appendChild(iNodeButtonBackText);
             aNodeButtonBack.appendChild(iNodeButtonBack);
             this.buttonNode.appendChild(aNodeButtonBack);
@@ -412,6 +582,23 @@
             return url;
         }
     };
+    //========================== Data Access Object ====================================
+    const dao = {
+        lesen: function(){
+            if (localStorage.getItem('myPersonalFavorites')){
+                let text = localStorage.getItem('myPersonalFavorites');
+                let daten = JSON.parse(text);
+                return daten;
+            } else {
+                return [];
+            }
+        },
+        schreiben:function(daten){
+            let text = JSON.stringify(daten);
+            localStorage.setItem('myPersonalFavorites', text);
+        }
+    };
+
     //========================== App ====================================
     presenter.init();
 })();
